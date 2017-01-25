49e0a29ea6a0ed5ac8df09cf75a11a5047aa6ba5
sibowsb <sibo.wsb+accounts@gmail.com>
2017-01-25 00:10:30 -0600

    today

--- scanner.py
+++ scanner.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 import numpy as np
 from matplotlib import pyplot as plt
 import pylab as pl
@@ -7,7 +8,13 @@ from pylibdmtx.pylibdmtx import decode
 
 REF_PT_RATI0 =1.053
 NUM_OPTIONS = 5
-MARKED_THRESH = 1.5
+MARKED_THRESH = 0.175
+MAGIC_ROW = 25
+MAGIC_COL = [18, 43, 67, 91, 115]
+MAGIC_SPAN = 6
+SKIP = 2
+NAME = [120, 175, 115, 495]
+CLASS = [120, 175, 565, 665]
 
 
 class RawPhoto:
@@ -24,6 +31,8 @@ class RawPhoto:
         self.th = cv2.adaptiveThreshold(self.img, 255,
                                         cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                         1, 29, 8)
+        print(os.getcwd())
+        # cv2.imwrite("self_th.png", self.th)
         # Find contours
         contours0, hir = cv2.findContours(self.th, cv2.RETR_LIST,
                                           cv2.CHAIN_APPROX_SIMPLE)
@@ -114,11 +123,12 @@ class PaperScan:
     paper_id = None
     correct_ans = []
     ans_imgs = []
+    ans_imgs_o = []
     num_questions = 60
     name_img = None
     class_img = None
     marked_ans = []
-    percent = -1
+    stat = []
 
 
     def __init__(self, paper_img, num_questions=60):
@@ -143,16 +153,23 @@ class PaperScan:
         self.test_id = content[:5]
         self.paper_id = content[5:]
 
-    def read_answers(self):
-        pass
+    def get_name_pics(self):
+        # cv2.imwrite("formated_paper.png", self.img)
+        self.name_img = self.img[NAME[0]:NAME[1], NAME[2]:NAME[3]]
+        self.class_img = self.img[CLASS[0]:CLASS[1], CLASS[2]:CLASS[3]]
+        # cv2.imwrite("name.png", self.name_img)
+        # cv2.imwrite("class.png", self.class_img)
 
     def remove_edges(self):
         scan_times = 22
         threshold = 0.5
         count = 0
         ans_img_trim = []
-        for ans_img in self.ans_imgs:
-
+        ans_img_o_trim = []
+        for i in range(len(self.ans_imgs)):
+            # for ans_img in self.ans_imgs:
+            ans_img = self.ans_imgs[i]
+            ans_img_o = self.ans_imgs_o[i]
             # cv2.imwrite("ans-img-trim-%d.png" % count, ans_img)
 
             h, w = ans_img.shape[:2]
@@ -204,15 +221,19 @@ class PaperScan:
                 r = 0
 
             trimmed = ans_img[t:h - b, l:w - r]
-            kernel = np.ones((2, 2), np.uint8)
-            # erosion = cv2.erode(trimmed, kernel, iterations=1)
-            opening = cv2.morphologyEx(trimmed, cv2.MORPH_OPEN, kernel)
+            trimmed_o = ans_img_o[t:h - b, l:w - r]
+            # kernel = np.ones((2, 2), np.uint8)
+            # # erosion = cv2.erode(trimmed, kernel, iterations=1)
+            # opening = cv2.morphologyEx(trimmed, cv2.MORPH_OPEN, kernel)
 
             # TODO: delete this
-            cv2.imwrite("ans-img-trim-%d.png" % count, opening)
+            # cv2.imwrite("ans-img-trim-o-%d.png" % count, trimmed_o)
             count += 1
-            ans_img_trim.append(ans_img[t:h - b, l:w - r])
+            ans_img_trim.append(trimmed)
+            ans_img_o_trim.append(trimmed_o)
+            # ans_img_o_trim.append()
 
+        self.ans_imgs_o = ans_img_o_trim
         self.ans_imgs = ans_img_trim
 
     def segment(self):
@@ -226,70 +247,72 @@ class PaperScan:
             left = col_keys[col][0] - offset
             right = col_keys[col][1] + offset
             ans_img = self.th_inv[up:down, left:right]
+            ans_img_o = self.img[up:down, left:right]
             self.ans_imgs.append(ans_img)
-            cv2.imwrite("ans-img-%d.png" % i, ans_img)
+            self.ans_imgs_o.append(ans_img_o)
+            # cv2.imwrite("ans-img-%d.png" % i, ans_img)
 
+    def read_ans(self):
+        answers = []
+        for img_to_grade in self.ans_imgs_o:
+            answer = magic_read_ans(img_to_grade)
+            answers.append(answer)
+        self.marked_ans = answers
 
-    def read_filled_answers(self):
-        for curr_img in self.ans_imgs:
-            h, w = curr_img.shape[:2]
-            partition_len = int(w / NUM_OPTIONS);
-            brightnesses = [0] * NUM_OPTIONS
-            for i in range(NUM_OPTIONS):
-                for j in range(h):
-                    begin_partition = partition_len * i
-                    end_partition = partition_len * (i + 1)
-                    for k in range(begin_partition, end_partition):
-                        for row in range(h):
-                            for col in range(partition_len):
-                                brightnesses[i] += curr_img[row][col]
-            min = min(brightnesses)
-            res = []
-            for i in range(NUM_OPTIONS):
-                if brightnesses[i] > MARKED_THRESH * min:
-                    res.append(i)
-            if len(res) == 0:
-                res = None
-            elif len(res) == 1:
-                res = res[0]
-            self.marked_ans.append(res)
-
-    def get_test_info(self):
-        pass
-        # TODO
-
-    def grade(self, grading_function=None):
-        if len(self.correct_ans) == 0:
-            print("No standard answer found")
-            # TODO
-        if grading_function == None:
-            self.default_grading()
-        else:
-            pass
-            # TODO
-
-
-    def default_grading(self, toPrint=False):
-        # TODO: multiple answers
-        sum = 0
-        for i in range(len(self.correct_ans)):
+    def grade(self):
+        correctAnswers = 0
+        breakdown = []
+        for i in range(self.num_questions):
             if self.correct_ans[i] == self.marked_ans[i]:
-                sum += 1
-        percent = float(sum) / len(self.correct_ans)
-        if toPrint:
-            print("Grade of paper id %s is %d / %d. Percentage mark: %2f"
-                  % (self.paper_id, sum, len(self.correct_ans), percent))
-        self.percent = percent
-        return percent
+                breakdown.append(1)
+                correctAnswers += 1
+            else:
+                breakdown.append(0)
+        mark = 100 * float(correctAnswers) / self.num_questions
+        self.stat = [breakdown, mark]
+
+
+def magic_read_ans(img):
+    dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E'}
+    sums = []
+    ans = []
+    for k in range(NUM_OPTIONS):
+        i, j = -MAGIC_SPAN, -MAGIC_SPAN
+        this_sum = 0
+        while i < MAGIC_SPAN:
+            while j < MAGIC_SPAN:
+                this_sum += img[MAGIC_ROW + i][MAGIC_COL[k] + j]
+                j += SKIP
+            i += SKIP
+        sums.append(this_sum)
+    sum_of_sums = sum(sums)
+    for i in range(NUM_OPTIONS):
+        if sums[i] < sum_of_sums * MARKED_THRESH:
+            ans.append(dict[i])
+    return ans
 
 
 
 if __name__ == "__main__":
 
-    test_img = cv2.imread("source7.jpeg", 0)
+    test_img = cv2.imread("source10.png", 0)
     rp = RawPhoto(test_img)
-    rp.get_papers(4, 22)
-    rp.paper_objs[1].segment()
-    rp.paper_objs[1].remove_edges()
-    rp.paper_objs[1].read_filled_answers()
-    print(rp.paper_objs[1].marked_ans)
+    rp.get_papers(2, 60)
+    rp.paper_objs[0].segment()
+    rp.paper_objs[0].remove_edges()
+    # rp.paper_objs[0].read_filled_answers()
+    # print(rp.paper_objs[1].marked_ans)
+    rp.paper_objs[0].read_ans()
+    rp.paper_objs[0].correct_ans = [['C'], ['C'], ['D'], ['D'], ['A'], ['C'],
+                                    ['B'], ['A'], ['C'], ['D'], ['D'], ['A'],
+                                    ['C'], ['B'], ['A'], ['C'], ['D'], ['D'],
+                                    ['A'], ['C'], ['B'], ['A'], ['C'], ['D'],
+                                    ['D'], ['A'], ['C'], ['B'], ['A'], ['C'],
+                                    ['D'], ['D'], ['A'], ['C'], ['B'], ['A'],
+                                    ['C'], ['D'], ['D'], ['A'], ['C'], ['B'],
+                                    ['A'], ['C'], ['D'], ['D'], ['A'], ['C'],
+                                    ['B'], ['A'], ['C'], ['D'], ['D'], ['A'],
+                                    ['C'], ['B'], ['A'], ['C'], ['D'], ['D']]
+    rp.paper_objs[0].grade()
+    print(rp.paper_objs[0].stat)
+    rp.paper_objs[0].get_name_pics()
